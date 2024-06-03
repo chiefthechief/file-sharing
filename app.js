056b2a937a73379ea3bcb08f3634468fcee81734
unknown <enamcudjoeahiable@gmail.com>
2024-06-03 11:44:26 +0000

    port issues

--- app.js
+++ app.js
@@ -46,6 +46,6 @@ async function handleDownload(req, res) {
     res.download(file.path, file.originalName)
 }
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is live")
 })
